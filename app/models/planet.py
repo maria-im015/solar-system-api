ab719b8b5db485970a094a1d0ee22745ee26c1b1
sneha agarwal <sneha8412@gmail.com>
2021-04-30 11:48:45 -0700

    created planet model and database

---
+++ app/models/planet.py
@@ -0,0 +1,19 @@
+from app import db
+
+
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String)
+    description = db.Column(db.String) #why dont we specify the type here?
+    order = db.Column(db.Integer)
+    
+    def to_json(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+            "order":self.order
+        }
+    
+    def to_string(self):
+        return f"{self.id}: {self.name} Description: {self.description} Order from the star {self.order} "
--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,7 @@
 from flask import Blueprint
 
+planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
+
+@planets_bp.route("/<planets_order>", methods=["GET"], strict_slashes=False)
+def get_single_planet(planets_order):
+    planet = planet
